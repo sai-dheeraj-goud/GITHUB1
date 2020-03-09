6ea0a420ddb5be4a37d0fb5490e1a56017ba7c08
BLTuser <BLTuser@BLT225>
2020-03-09 10:35:04 +0530

    l,,l  mjijijoio

---
+++ src/n1t1/ntest1.java
@@ -0,0 +1,14 @@
+package n1t1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ntest1 {
+	@Test
+	public void f() {
+	System.setProperty("webdriver.chrome.driver","chromedriver_v79.exe");
+	WebDriver dr=new ChromeDriver();
+	dr.get("http://demowebshop.tricentis.com/");
+
+}}
